cef1c50b3072addc153a8217c1dad22d67564092
zyx0015 <139255115+zyx0015@users.noreply.github.com>
2023-10-31 01:23:02 +0800

    Add files via upload

--- maponline.py
+++ maponline.py
@@ -26,10 +26,10 @@ col1, col2 = st.columns(2)
 with col1:
     st.title("有段石锛")
     file_name = st.file_uploader("import your csv file")
-
+    current_time = st.slider("BP",0,8000,5000,100)
 
 if file_name is not None:
-    current_time = st.slider("BP",0,8000,5000,100)
+    
     #写一个函数，读取csv并且根据时间筛选
     df = filter_sites_by_era(file_name,current_time)  # 读取上传的 CSV 文件
     # 创建地图对象
